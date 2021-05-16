92a71654804b638b2d9d1ddeb8459784ff8473b0
Floriane Lefebvre <flo.le@icloud.com>
2021-05-16 21:43:57 +0200

    port

--- index.js
+++ index.js
@@ -15,25 +15,6 @@ app.use(charactersRoutes);
 const comicsRoutes = require("./routes/comics");
 app.use(comicsRoutes);
 
-// app.get("/comics/:id", async (req, res) => {
-
-//     const { limit, skip, name } = req.fields;
-//     console.log(limit, skip, name);
-
-//     try {
-//         const response = await axios.get(
-//             `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&skip=${skip}&limit=${limit}&name=${name}`
-//         );
-//         res.status(200).json(response.data);
-//     } catch (error) {
-//         res.status(400).json(error.message);
-//     }
-// });
-
-// app.all("*", (req, res) => {
-//     res.status(400).json({ message: "Page not found" });
-// });
-
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server has started");
 });
